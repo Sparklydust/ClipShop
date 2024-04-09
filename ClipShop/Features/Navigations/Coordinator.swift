5b8eb348eb0b1d0bac224dcfa38e680bb44f3f7a
Roland Lariotte <reel.errors.0z@icloud.com>
2024-04-09 13:34:46 +0200

    Setup the Coordinator with CoordinatorProtocol and create a start navigation

--- ClipShop/Features/Navigations/Coordinator.swift
+++ ClipShop/Features/Navigations/Coordinator.swift
@@ -2,9 +2,28 @@
 // Copyright © 2024 and confidential to ClipShop. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 /// Coordinate the navigations flows between views within the application.
-final class Coordinator {
-  // Intentionally empty
+final class Coordinator: CoordinatorProtocol {
+
+  var navigationController: UINavigationController
+  var shopListViewModel: ShopListViewModel
+
+  init(
+    navigationController: UINavigationController,
+    shopListViewModel: ShopListViewModel = ShopListViewModel()
+  ) {
+    self.navigationController = navigationController
+    self.shopListViewModel = shopListViewModel
+  }
+}
+
+// MARK: - Navigations
+extension Coordinator {
+
+  func start() {
+    let viewController = ShopListViewController(viewModel: shopListViewModel)
+    navigationController.pushViewController(viewController, animated: true)
+  }
 }
--- ClipShopTests/Cases/Features/Navigations/CoordinatorTests.swift
+++ ClipShopTests/Cases/Features/Navigations/CoordinatorTests.swift
@@ -5,17 +5,37 @@
 import XCTest
 @testable import ClipShop
 
-final class CoordinatorTests: XCTestCase {
+final class CoordinatorTests: BaseXCTestCase {
 
   var sut: Coordinator!
 
+  var shopListViewModelFake: ShopListViewModel!
+
   override func setUp() async throws {
     try await super.setUp()
-    sut = Coordinator()
+    shopListViewModelFake = ShopListViewModel(server: serverDummy)
+
+    sut = Coordinator(
+      navigationController: navigationControllerDummy,
+      shopListViewModel: shopListViewModelFake
+    )
   }
 
   override func tearDown() async throws {
     sut = nil
+    shopListViewModelFake = nil
     try await super.tearDown()
+  }
+
+  func testNavigations_pushesShopListViewController_resultIsTrue() {
+    sut = Coordinator(
+      navigationController: navigationControllerSpy,
+      shopListViewModel: shopListViewModelFake
+    )
+
+    sut.start()
+    let result = navigationControllerSpy.viewController is ShopListViewController
+
+    XCTAssertTrue(result, "Coordinator must push `ShopListViewController` in the navigation stack on `start()`.")
   }
 }
