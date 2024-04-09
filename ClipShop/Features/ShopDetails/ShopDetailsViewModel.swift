0d3f2de9dcec4bea97f4a130a5d9e059aa6b8bfb
Roland Lariotte <reel.errors.0z@icloud.com>
2024-04-09 15:36:31 +0200

    Create the ShopDetailsViewModel

---
+++ ClipShop/Features/ShopDetails/ShopDetailsViewModel.swift
@@ -0,0 +1,10 @@
+//
+// Copyright © 2024 and confidential to ClipShop. All rights reserved.
+//
+
+import Foundation
+
+/// Responsible of handling the logic of the ``ShopDetailsViewController``.
+final class ShopDetailsViewModel {
+  // Intentionally empty
+}
---
+++ ClipShopTests/Cases/Features/ShopDetails/ShopDetailsViewModelTests.swift
@@ -0,0 +1,21 @@
+//
+// Copyright © 2024 and confidential to ClipShop. All rights reserved.
+//
+
+import XCTest
+@testable import ClipShop
+
+final class ShopDetailsViewModelTests: BaseXCTestCase {
+
+  var sut: ShopDetailsViewModel!
+
+  override func setUp() async throws {
+    try await super.setUp()
+    sut = ShopDetailsViewModel()
+  }
+
+  override func tearDown() async throws {
+    sut = .none
+    try await super.tearDown()
+  }
+}
