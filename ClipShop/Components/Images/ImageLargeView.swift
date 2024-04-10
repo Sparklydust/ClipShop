2a4bb732da8bcf0f66218ad54cabc5b223f159d0
Roland Lariotte <reel.errors.0z@icloud.com>
2024-04-10 09:45:05 +0200

    Create the ImageLargeView component

---
+++ ClipShop/Components/Images/ImageLargeView.swift
@@ -0,0 +1,27 @@
+//
+// Copyright © 2024 and confidential to ClipShop. All rights reserved.
+//
+
+import UIKit
+
+/// Large image view to be presented on views to showcase an item.
+final class ImageLargeView: UIImageView {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.setupImage()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  /// Setup the image attributes.
+  private func setupImage() {
+    translatesAutoresizingMaskIntoConstraints = false
+    contentMode = .scaleAspectFill
+    layer.cornerRadius = 12
+    clipsToBounds = true
+  }
+}
