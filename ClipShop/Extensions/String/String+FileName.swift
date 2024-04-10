3d2726cae9357c00891b84dd5e69b73a9279da69
Roland Lariotte <reel.errors.0z@icloud.com>
2024-04-10 13:03:49 +0200

    Fix image naming in cache

--- ClipShop/Extensions/String/String+FileName.swift
+++ ClipShop/Extensions/String/String+FileName.swift
@@ -5,10 +5,16 @@
 import Foundation
 
 extension String {
-  
+
   /// Define a file name from a URL to be used within ``FileManager``.
   var extractedFileName: String {
     let baseUrl = self.components(separatedBy: "?").first ?? self
-    return baseUrl.components(separatedBy: "/").last ?? String()
+    let pathComponents = baseUrl.components(separatedBy: "/")
+    guard pathComponents.count > 2
+    else { return baseUrl.components(separatedBy: "/").last ?? String() }
+
+    let sizeComponent = pathComponents[pathComponents.count - 2]
+    let nameComponent = pathComponents.last ?? String()
+    return sizeComponent + "-" + nameComponent
   }
 }
